042cdf8168db4da1ee3521bf8d509b55260b8457
Youssef <youssefosamasalah51@gmail.com>
2026-06-09 15:02:54 +0300

    I've started working on the Javascript So far I've buitl a system the randomly selects questions set up the logic to move to the next question when the user answers and added the navigation to the end scerrn once the quiz is finished.

---
+++ game.js
@@ -0,0 +1,80 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName('choice-text'))
+
+let currentQuestion = {};
+let acceptingAnswers = false ;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+
+let questions = [
+    {
+        question: "inside which html element do we put the jave",
+        choice1: "<script>",
+        choice2: "<javascript>",
+        choice3: "<js>",
+        choice4: "<scripting>",
+        answer: 1
+    },
+    {
+        question: "what is the correct syntax for referring to and etenal scrotp",
+        choice1: "<script href = 'xxx.js'>",
+        choice2: "<script name = 'xxx.js'>",
+        choice3: "<script src = 'xxx.js'>",
+        choice4: "<script file = 'xxx.js'>",
+        answer: 3
+    },
+    {
+        question: "how do you  write 'hello world' in an alert box?",
+        choice1: "<msgBox('Hello world')>",
+        choice2: "<alertBox('Hello world')>",
+        choice3: "<msg('Hello world')>",
+        choice4: "<alert('Hello world')>",
+        answer: 4
+    },
+]
+
+// make m game is life
+
+const CORRECT_BOUNUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter =0;
+    score = 0 ;
+    availableQuesions = [...questions];
+    console.log (availableQuesions);
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    if (availableQuesions.length == 0 || questionCounter >= MAX_QUESTIONS){
+        return window.location.assign("end.html")
+    }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random()* availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
+    question.innerText = currentQuestion.question;
+    choices.forEach (choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion ['choice' + number]
+    })
+    availableQuesions.splice(questionIndex , 1);
+    acceptingAnswers =true;
+};
+
+choices.forEach(choice => {
+    choice.addEventListener("click" , e =>{
+        if (!acceptingAnswers) return;
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+        console.log(selectedAnswer);
+        getNewQuestion();
+    });
+});
+
+
+
+startGame()
+
