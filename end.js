b511d6d7e813e5c28bb014a06eb2f46f37f1b900
Youssef <youssefosamasalah51@gmail.com>
2026-06-11 16:29:09 +0300

    I built a high scores page that ranks players from highest score to lowest I also created  a system to display those  scores on tha page the coe isself isn't too coplicted but comig up with thd logic behind it took some thought.At this point the project is nearly finished there are still alot of final touches iwould like to add especially integraiong an API to fetch real queations and make the quiz more dymamic and evgaging

--- end.js
+++ end.js
@@ -3,7 +3,7 @@ const saveScoreBtn = document.getElementById('saveScoreBtn')
 const finalScore = document.getElementById('finalScore')
 const mostRecentScore = localStorage.getItem('mostRecentScore')
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-const MAX_HIGH_SCORES = 5 ;
+const MAX_HIGH_SCORES = 7 ;
 
 finalScore.innerText = mostRecentScore
 
@@ -23,7 +23,7 @@ saveHighScore = e => {
     
     highScores.push(score);
     highScores.sort((a,b) => b.score - a.score);
-    highScores.splice(5)
+    highScores.splice(MAX_HIGH_SCORES)
     localStorage.setItem("highScores",JSON.stringify(highScores));
     window.location.assign("index.html")
 };
